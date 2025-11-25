5765466ac503af9886b7a0d7c8508cd620677054
Shorif <shorifsimec@gmail.com>
2025-11-25 12:33:52 +0600

    selected menu has background colour

--- resources/js/pages/contact/contact-us.tsx
+++ resources/js/pages/contact/contact-us.tsx
@@ -1,9 +1,18 @@
 import AppLayout from '@/layouts/app-layout';
+import { BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
 export default function ContactUs() {
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: 'Contact Us',
+            href: '/comtact-us',
+        },
+    ];
+
     return (
-        <AppLayout>
+        <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Contact Us" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <h1>welcome contact</h1>
